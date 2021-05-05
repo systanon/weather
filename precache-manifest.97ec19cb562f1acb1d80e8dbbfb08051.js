5faf940c2b7e874443672477050e085bef1301f7
systanon (via Travis CI) <tustanovskiis@gmail.com>
2021-05-05 16:57:24 +0000

    Deploy systanon/weather to github.com/systanon/weather.git:gh-pages

---
+++ precache-manifest.97ec19cb562f1acb1d80e8dbbfb08051.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a8b7b466ff0fe8b1ed",
+    "url": "/weather/css/chunk-d4c1104e.b57fe8ba.css"
+  },
+  {
+    "revision": "7350ca8d502cafbf0d3c",
+    "url": "/weather/css/chunk-vendors.914d6261.css"
+  },
+  {
+    "revision": "e24e2235bdc02eeb7e630856700e404a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "1ee837abd0182d81e8e1",
+    "url": "/weather/js/app.5b90848e.js"
+  },
+  {
+    "revision": "53a8b7b466ff0fe8b1ed",
+    "url": "/weather/js/chunk-d4c1104e.1b518a0c.js"
+  },
+  {
+    "revision": "7350ca8d502cafbf0d3c",
+    "url": "/weather/js/chunk-vendors.e4e50d13.js"
+  },
+  {
+    "revision": "3330becfb84ab93a6edc",
+    "url": "/weather/js/city.74fe955b.js"
+  },
+  {
+    "revision": "458234fc6be47cef7c9146de12cc7556",
+    "url": "/weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather/robots.txt"
+  }
+]);
